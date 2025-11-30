06a4919f2b485f19b9f83a277f44e5f5f1d58a3a
liuhelei23 <liuhelei23@mails.tsinghua.edu.cn>
2025-11-30 20:00:04 +0800

    feat: 在main中加入词法和语法分析，生成ANTLR的语法分析树

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,31 @@
 #include <iostream>
+#include <fstream>
+#include <any>
+#include "antlr4-runtime.h"
+#include "SysYLexer.h"
+#include "SysYParser.h"
 
-int main(int argc, const char *argv[]) {
-  if (argc < 3) {
+using namespace antlr4;
+
+int main(int argc, const char *argv[])
+{
+  if (argc < 3)
+  {
     std::cerr << "Usage: ./compiler <input-file> <output-file>"
               << std::endl;
     return 1;
   }
+
+  std::ifstream stream;
+  stream.open(argv[1]);
+
+  ANTLRInputStream input(stream);            // 创建 ANTLRInputStream 对象
+  SysYLexer lexer(&input);                   // 创建 SysYLexer 对象
+  CommonTokenStream tokens(&lexer);          // 创建 CommonTokenStream 对象
+  SysYParser parser(&tokens);                // 创建 SysYParser 对象
+  tree::ParseTree *tree = parser.compUnit(); // 获取 ANTLR 语法分析树
+
   // TODO: Implement the main function of the compiler.
+
   return 0;
 }

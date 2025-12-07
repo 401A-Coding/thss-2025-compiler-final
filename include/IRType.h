befc3de5108d170ea02aa2f37f2c4853f32229dc
liuhelei23 <liuhelei23@mails.tsinghua.edu.cn>
2025-12-07 20:58:15 +0800

    feat: 增加IRType

---
+++ include/IRType.h
@@ -0,0 +1,21 @@
+// include/IRType.h（复用之前的类型系统，或简化版）
+#include <string>
+class IRType
+{
+public:
+    static IRType *getIntType()
+    {
+        static IRType t("i32");
+        return &t;
+    }
+    static IRType *getVoidType()
+    {
+        static IRType t("void");
+        return &t;
+    }
+    std::string toString() const { return typeStr; }
+
+private:
+    IRType(std::string s) : typeStr(s) {}
+    std::string typeStr;
+};
